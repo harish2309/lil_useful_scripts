4b6348c023e9c5b21abf6989f219f7fd1c8e6e51
harish2309 <36474847+harish2309@users.noreply.github.com>
2021-12-15 23:01:33 +0530

    Delete iplocation.sh

--- iplocation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-IP=$(curl -sS ifconfig.co)
-ccode=$(curl -sS ip-api.com/json/$IP | jq '.countryCode') 
-rname=$(curl -sS ip-api.com/json/$IP | jq '.regionName')
-colls=$(echo "$ccode, $rname" | sed 's/\"//g')
-echo "<txt><span weight=\"bold\" fgcolor=\"#432ADF\">"  $colls"</span></txt>"
-echo "<tool></tool>"
